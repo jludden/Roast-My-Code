88f46ab3ddcc1d50fe0d5cc6f947ca8887a0e4b7
jludden <jasonludden@gmail.com>
2020-01-11 17:29:23 +0800

    first FQL custom resolver function

---
+++ src/components/CommentableDocument/EndpointTest.tsx
@@ -0,0 +1,56 @@
+import * as React from 'react';
+import * as R from 'ramda';
+import ApolloClient, { gql, ExecutionResult } from 'apollo-boost';
+import { useQuery, useMutation } from '@apollo/react-hooks';
+import {
+    Section,
+    Title,
+    Tag,
+    Container,
+    Input,
+    Button,
+    Block,
+    Help,
+    Control,
+    Delete,
+    Field,
+    Panel,
+    Checkbox,
+    Icon,
+    Progress,
+} from 'rbx';
+
+const TEST_FQL_ENDPOINT = gql`
+    query test($name: String!) {
+        sayHello(name: $name)
+    }
+`;
+
+export const EndpointTest = () => {
+    const [showTest, setShowTest] = React.useState(false);
+
+    return (
+        <>
+            <Button color="info" onClick={() => setShowTest(!showTest)}>
+                Test Endpoint
+            </Button>
+            {showTest && <EndpointTestDisplay />}
+        </>
+    );
+};
+
+export const EndpointTestDisplay = () => {
+    // Load Repo
+    const name = 'Jason';
+    const { data, error, loading, client, refetch } = useQuery<any>(TEST_FQL_ENDPOINT, {
+        variables: { name },
+    });
+
+    return (
+        <>
+            <Title> hello world</Title>
+            {loading && <p>loading..</p>}
+            {!loading && data && <p>{data.sayHello}</p>}
+        </>
+    );
+};
