5c011c5229b8e0ca3c4185183baee16512cebee1
Jason Ludden <jasonludden@gmail.com>
2020-08-12 00:49:35 +0000

    fix merge

--- src/components/CommentableDocument/DocumentBody.tsx
+++ src/components/CommentableDocument/DocumentBody.tsx
@@ -13,8 +13,6 @@ import * as React from 'react';
 import '../../App.css';
 import createElement from 'react-syntax-highlighter/dist/cjs/create-element';
 
-import createElement from 'react-syntax-highlighter/dist/cjs/create-element';
-
 // import tomorrow from 'react-syntax-highlighter/dist/styles/prism/tomorrow';
 // import ghcolors from 'react-syntax-highlighter/dist/styles/prism/ghcolors';
 // import darcula from 'react-syntax-highlighter/dist/styles/prism/darcula';
