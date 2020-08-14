4c2ecaa2011d81061234c0da813b9663a3dfea30
Jason Ludden <jasonludden@gmail.com>
2020-08-14 14:18:17 +0000

    update error  boundary

--- src/components/Common/ErrorBoundary.tsx
+++ src/components/Common/ErrorBoundary.tsx
@@ -1,24 +1,28 @@
 import React from 'react';
 
+interface IErrorBoundaryProps {
+    message?: string;
+}
+
 interface IErrorBoundaryState {
     hasError: boolean;
 }
 
 class ErrorBoundary extends React.Component<IErrorBoundaryProps, IErrorBoundaryState> {
-    constructor(props) {
+    constructor(props: IErrorBoundaryProps) {
         super(props);
         this.state = { hasError: false };
     }
 
-    static getDerivedStateFromError(error) {
+    static getDerivedStateFromError(error: Error): IErrorBoundaryState {
         // Update state so the next render will show the fallback UI.
         return { hasError: true };
     }
 
-    componentDidCatch(error, errorInfo) {
+    componentDidCatch(error: Error, errorInfo: any): void {
         // You can also log the error to an error reporting service
         //   logErrorToMyService(error, errorInfo);
-        console.log();
+        console.log(`${error}, ${errorInfo}`);
     }
 
     render() {
