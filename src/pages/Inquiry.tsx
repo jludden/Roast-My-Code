fb30165f0311dd7e2f530f37bb0d1cec8a33dc29
Jason Ludden <jasonludden@gmail.com>
2020-11-22 16:23:52 +0000

    test netlify forms 2

--- src/pages/Inquiry.tsx
+++ src/pages/Inquiry.tsx
@@ -6,13 +6,7 @@ const Inquiry = () => {
     return (
         <div>
             <h2>Report an issue</h2>
-            <p>
-                This application, created mostly by copy/pasting stack overflow code, was my first big foray into React and Web development.
-                I don't think it's great, but hey at least I built something.
-                Check out the code for the app within this app! Or you can directly view it in Github:            
-                <a href="https://github.com/jludden/react-ts-tdd"/>
-            </p>
-            <form name="report-issue" method="POST" data-netlify="true" netlify-honeypot="bot-field">
+            <form name="report-issue" method="POST">
                 <input type="hidden" name="form-name" value="report-issue" />
                 <p style={{ display: "none" }}>
                     <label>Don’t fill this out if you're human: <input name="bot-field" /></label>
