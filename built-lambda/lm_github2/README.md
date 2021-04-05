ae6fac4a9b11173f15a2a915b635fb49a1f522fd
Jason Ludden <jasonludden@gmail.com>
2021-04-05 01:13:14 +0000

    use env var for lambda url configuration

--- built-lambda/lm_github2/README.md
+++ built-lambda/lm_github2/README.md
@@ -2,12 +2,12 @@
 
 Used by Roast My Code to hide the Github API key
 
-# Setup
+### Setup
 Create an API Key with Github, then set it as an environment variable 
 named RMC_GITHUB_PAT in lambda
 Serverless has been configured to read the environment variable from secrets.yml
 
-### Debugging
+### Debugging locally
 A great way to debug this in VSCode is with:
 https://code.visualstudio.com/docs/nodejs/nodejs-debugging#_javascript-debug-terminal
 then using the opened terminal to execute: 
@@ -15,6 +15,33 @@ then using the opened terminal to execute:
 or simply
 `$ yarn test`
 
-# Deploying
+### Deploying to Lambda
 `serverless deploy`
 Make sure to set RMC_GITHUB_PAT environment variable in Lambda console
+
+### Testing in Api Gateway console
+POST /graphql
+Here is a sample header and body to test with:
+
+Headers:
+```
+authority: roast-my-code.com
+method: POST
+path: /.netlify/functions/repo_github
+scheme: https
+accept: */*
+accept-encoding: gzip, deflate, br
+accept-language: en-US,en;q=0.9
+content-length: 1218
+content-type: application/json
+origin: https://roast-my-code.com
+referer: https://roast-my-code.com/
+sec-fetch-dest: empty
+sec-fetch-mode: cors
+sec-fetch-site: same-origin
+```
+
+body:
+```
+{"operationName":"SearchMostTop10Star","variables": {"queryString":"js","first":5},"query":"query SearchMostTop10Star($queryString: String!, $first: Int) {  search(query: $queryString, type: REPOSITORY, first: $first) {    repositoryCount    edges {      node { ... on Repository { nameWithOwner name id databaseId createdAt descriptionHTML url resourcePath updatedAt defaultBranchRef { name __typename } owner { id login __typename } primaryLanguage { name color __typename } languages(first: 5) { nodes { name __typename } __typename } descriptionHTML stargazers { totalCount __typename } forks { totalCount __typename } __typename } __typename } __typename } __typename }}" }
+```
--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ import ApolloClient from 'apollo-boost';
 import { InMemoryCache, IntrospectionFragmentMatcher } from 'apollo-cache-inmemory';
 import IntrospectionResultData, { Blob, Repository, RepositoryConnection } from './generated/graphql';
 import { Search } from './pages/Search';
-import { Home} from './pages/Home';
+import { Home } from './pages/Home';
 import About from './pages/About';
 import Inquiry from './pages/Inquiry';
 import CommentableCode from './components/CommentableCodePage/CommentableCode';
@@ -25,64 +25,58 @@ import { SigninModal } from './components/FirebaseChat/SigninModal';
 import { UserDetailsModal } from './components/FirebaseChat/LoggedInStatus';
 import Signup from './pages/Signup';
 
-
 const fragmentMatcher = new IntrospectionFragmentMatcher({
     introspectionQueryResultData: IntrospectionResultData,
 });
 export const cache = new InMemoryCache({ fragmentMatcher });
 
-    export const githubClient: ApolloClient<InMemoryCache> = new ApolloClient({
-        cache,
-        uri: '/.netlify/functions/repo_github',
+export const githubClient: ApolloClient<InMemoryCache> = new ApolloClient({
+    cache,
+    uri: process.env.REACT_APP_RMC_GITHUB_LAMBDA_URL,
 });
 
 export const App = () => {
-    // // <div className="App_Background">
-
-        const url = 'https://jludden-react.netlify.com/';
-
     return (
+        <>
+            <ApolloProvider client={githubClient as any}>
+                <Router>
+                    <QueryParamProvider ReactRouterRoute={Route}>
+                        <CCNavBar />
 
-            <>
-                <ApolloProvider client={githubClient as any}>
-                        <Router>
-                            <QueryParamProvider ReactRouterRoute={Route}>
-                                <CCNavBar />
+                        <SigninModal />
+                        <UserDetailsModal />
 
-                                <SigninModal />
-                                <UserDetailsModal />
-
-                                {/* <Section color="dark"> */}
-                                    <Switch>
-                                        <Route path="/" exact component={Home} />
-                                        <Route path="/search" component={Search} />
-                                        <Route path="/about/" component={About} />
-                                        <Route path="/repo/" component={CommentableCodePage} />
-                                        <Route path="/chat/" component={FirebaseChat} />
-                                        <Route path="/inquiry" component={Inquiry} />
-                                        {/* <Route path="/signup/" component={Signup} />
+                        {/* <Section color="dark"> */}
+                        <Switch>
+                            <Route path="/" exact component={Home} />
+                            <Route path="/search" component={Search} />
+                            <Route path="/about/" component={About} />
+                            <Route path="/repo/" component={CommentableCodePage} />
+                            <Route path="/chat/" component={FirebaseChat} />
+                            <Route path="/inquiry" component={Inquiry} />
+                            {/* <Route path="/signup/" component={Signup} />
                                         <Route path="/login/" component={Login} /> */}
-                                        {/* //  TODO only render signup, login if authenticated not true!
+                            {/* //  TODO only render signup, login if authenticated not true!
                                             // render={(props) => authenticated === false
                                             //     ? <Component {...props} />
                                             //     : <Redirect to='/chat' />}
                                             // />
                                         */}
-                                    </Switch>
-                                {/* </Section> */}
-                            </QueryParamProvider>
-                        </Router>
-                        <AppFooter />
-                </ApolloProvider>
-            </>
+                        </Switch>
+                        {/* </Section> */}
+                    </QueryParamProvider>
+                </Router>
+                <AppFooter />
+            </ApolloProvider>
+        </>
     );
-}
+};
 
 export const AppContext = () => (
     <FirebaseCommentsProvider>
         <App />
     </FirebaseCommentsProvider>
-)
+);
 
 export default AppContext;
 
@@ -92,18 +86,15 @@ const AppFooter = () => {
             <Content textAlign="centered">
                 <p>&copy; Jason Ludden 2021</p>
             </Content>
-            {/* <Section>
-                <FirebaseQuery />
-            </Section> */}
         </Footer>
-    )
-}
+    );
+};
 
 function CommentableCodePage() {
     return (
         <div className="App">
             <Container breakpoint="desktop">
-                <ErrorBoundary >
+                <ErrorBoundary>
                     {/* <EndpointTest /> */}
                     <CommentableCode />
                 </ErrorBoundary>
@@ -111,60 +102,3 @@ function CommentableCodePage() {
         </div>
     );
 }
-
-type TParams = { id: string };
-
-function Index({ match }: RouteComponentProps<TParams>) {
-    return (
-        <>
-            <h2>
-                This is a page for product with ID:
-                {match.params.id}{' '}
-            </h2>
-            <Link to="/repo/">Commentable Code</Link>
-        </>
-    );
-}
-
-// function Index() {
-//   return
-//   <Link to="/users/">Users</Link>
-//     ;
-// }
-
-
-
-function Users() {
-    return <h2>Users</h2>;
-}
-
-
-function AppRouter() {
-    return (
-        <Router>
-            <div>
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to="/">Home</Link>
-                        </li>
-                        <li>
-                            <Link to="/search">Search</Link>
-                        </li>
-                        <li>
-                            <Link to="/about/">About</Link>
-                        </li>
-                        <li>
-                            <Link to="/users/">Users</Link>
-                        </li>
-                    </ul>
-                </nav>
-
-                <Route path="/" exact component={Index} />
-                <Route path="/about/" component={About} />
-                <Route path="/users/" component={Users} />
-            </div>
-        </Router>
-    );
-}
-
